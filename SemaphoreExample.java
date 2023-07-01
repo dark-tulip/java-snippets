8394bdc6f3b942c92552fcc03791e49f1d2e815c
dark-tulip <89765480+dark-tulip@users.noreply.github.com>
2023-07-01 12:30:04 +0600

    Create SemaphoreExample.java

---
+++ SemaphoreExample.java
@@ -0,0 +1,51 @@
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreExample {
+
+    public static void main(String[] args) {
+
+        Semaphore restRoom = new Semaphore(2);
+        new Person("name1", restRoom);
+        new Person("name2", restRoom);
+        new Person("name3", restRoom);
+        new Person("name4", restRoom);
+        new Person("name5", restRoom);
+    }
+}
+
+
+class Person extends Thread {
+
+    String name;
+
+    private Semaphore restRoom;
+
+    public Person(String name, Semaphore restRoom) {
+        this.name = name;
+        this.restRoom = restRoom;
+        this.start();
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " is waiting");
+
+            // ожидает попытки получения доступа к ресурсу
+            // до этого времени поток заблокирован
+            restRoom.acquire();
+            sleep(1000);
+            System.out.println(this.name + " in the room, available " + this.restRoom.availablePermits());
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+
+        } finally {
+
+            // увеличить счетчик семафора на единицу,
+            // тем самым разрешая следующему потоку получить доступ к ресурсу
+            restRoom.release();
+        }
+    }
+}
